eba4a351c17c9601c6d4afb2d11de82ef273428d
Pat D <49631522+putergeek7@users.noreply.github.com>
2020-01-17 05:52:38 -0800

    working on first eventListener

--- js/index.js
+++ js/index.js
@@ -1,1 +1,22 @@
 // Your code goes here
+
+// function (imgHome) {
+
+//     newCard = document.createElement('div');
+//     // newHeader = document.createElement('header');
+//     // newImg = document.createElement('div');
+//     // newPara = docue
+
+//     newCard.addEventListener("click", () => {
+//         newCard.classList.toggle("selected")
+//       })
+
+// }
+
+const advImg = document.querySelector('.img-fluid rounded');
+
+console.log(advImg);
+
+advImg.addEventListener('mouseenter', () => {
+	advImg.style.transform = "scale(2)";
+});
